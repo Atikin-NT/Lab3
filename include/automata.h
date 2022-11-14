9aab837445346f29ef45b7cdd2f837c00d10802c
Nikita Ivanov <ivanov.n.a.0620@gmail.com>
2022-11-14 15:33:07 +0300

    feat: add digit and operation function

---
+++ include/automata.h
@@ -0,0 +1,17 @@
+//
+// Created by atikin on 14.11.22.
+//
+
+#ifndef PARSER_AUTOMATA_H
+#define PARSER_AUTOMATA_H
+
+#include <string>
+#include <queue>
+#include "parser.h"
+using namespace std;
+
+void digit(char& c, string& tmp, queue<Lexema>& res, int& state);
+
+void operation(char& c, string& tmp, queue<Lexema>& res, int& state);
+
+#endif //PARSER_AUTOMATA_H
--- samples/parser_samples.cpp
+++ samples/parser_samples.cpp
@@ -1,8 +1,8 @@
 ﻿#include <string>
 #include <iostream>
-#include <stack>
 #include <queue>
 #include "parser.h"
+#include "automata.h"
 
 // Лексический
 // Синтаксический
@@ -12,54 +12,18 @@ using namespace std;
 queue <Lexema> lex(string input) {
     queue<Lexema>res;
     input += ' ';
-    int i = 0;
     string tmp = "";
-    string op = "+-*/()";
-    string sep = " \n\t";
     int state = 0;
-    for (i = 0; i < input.size(); i++) {
-        char c = input[i];
-        int fres;
+    for (char c : input) {
         switch (state)
         {
             case 0:
-                if (c >= '0' && c <= '9') {
-                    tmp = c;
-                    state = 1;
-                    break;
-                }
-                fres = op.find(c);
-                if (fres >= 0) {
-                    tmp = c;
-                    Lexema l(tmp, Operation);
-                    res.push(l);
-                    state = 0;
-                    break;
-                }
+                digit(c, tmp, res, state);
                 break;
             case 1:
-                if (c >= '0' && c <= '9') {
-                    tmp += c;
-                    state = 1;
-                    break;
-                }
-                fres = op.find(c);
-                if (fres >= 0) {
-                    Lexema l1(tmp, Value);
-                    res.push(l1);
-                    tmp = c;
-                    Lexema l2(tmp, Operation);
-                    res.push(l2);
-                    state = 0;
-                    break;
-                }
-                fres = sep.find(c);
-                if (fres >= 0) {
-                    Lexema l(tmp, Value);
-                    res.push(l);
-                    state = 0;
-                    break;
-                }
+                operation(c, tmp, res, state);
+                break;
+            default:
                 break;
         }
     }
